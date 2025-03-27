670a8c98378dfc5aa782441e7e38d109e31855d7
Ilya Kondryukov <mat8765@mail.ru>
2025-03-27 12:02:01 +0300

    исправил баг с запуском сидеров, добавим очистку перед началом. И добавил настройку в boot  для загрузки по https

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::truncate(); 
+        
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
